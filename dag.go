dc9e6d645bb11c2396cab698103f5e6cd43c1d58
wyz-shy <132677228+wyz-shy@users.noreply.github.com>
2024-03-05 19:41:34 +0800

    Update dag.go

--- dag.go
+++ dag.go
@@ -1,8 +1,40 @@
 package merkledag
 
-import "hash"
+import (
+	"hash"
+)
 
 func Add(store KVStore, node Node, h hash.Hash) []byte {
-	// TODO 将分片写入到KVStore中，并返回Merkle Root
-	return nil
+	if node.Type == "file" {
+		store.Save([]byte("file"), node.Data)
+		return hash(node.Data, h)
+	} else if node.Type == "dir" {
+		for _, child := range getDirContents(node) {
+			Add(store, child, h)
+		}
+		return hashDirContents(node, h)
+	}
+	return nil 
+}
+
+func hash(data []byte, h hash.Hash) []byte {
+	h.Reset()
+	h.Write(data)
+	return h.Sum(nil)
+}
+
+func getDirContents(dir Node) []Node {
+	return []Node{
+		{Type: "file", Data: []byte("file1")},
+		{Type: "file", Data: []byte("file2")},
+		{Type: "dir", Data: nil},
+	}
+}
+
+func hashDirContents(dir Node, h hash.Hash) []byte {
+	for _, child := range getDirContents(dir) {
+		childHash := Add(store, child, h)
+		h.Write(childHash)
+	}
+	return h.Sum(nil)
 }
