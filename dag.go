47569ce656140dc83911a425117222c01ccf5048
wyz-shy <132677228+wyz-shy@users.noreply.github.com>
2024-03-13 20:14:20 +0800

    Update dag.go

--- dag.go
+++ dag.go
@@ -16,21 +16,45 @@ type Object struct {
 }
 
 func Add(store KVStore, node Node, h hash.Hash) []byte {
-	switch node.Type() {
-	case FILE:
-		fileNode := node.(File)
-		store.Put([]byte("content"), fileNode.Bytes())
-		h.Write(fileNode.Bytes())
-		return h.Sum(nil)
-	case DIR:
-		dirNode := node.(Dir)
-		iterator := dirNode.It()
-		for iterator.Next() {
-			childNode := iterator.Node()
-			childMerkleRoot := Add(store, childNode, h)
-			h.Write(childMerkleRoot)
+	// TODO 将分片写入到KVStore中，并返回Merkle Root
+	switch n := node.(type) {
+	case File:
+		data := n.Bytes()
+		hashBytes := h.Sum(data)
+		//保存到store中
+		err := store.Put(hashBytes, data)
+		if err != nil {
+			panic(err)
 		}
-		return h.Sum(nil)
+		return hashBytes
+	case Dir:
+		links := make([]Link, 0)
+		it := n.It()
+		for it.Next() {
+			childNode := it.Node()
+			childHash := Add(store, childNode, h)
+			// 创建并添加链接到当前节点的Links字段中
+			links = append(links, Link{
+				Name: childNode.Name(),
+				Hash: childHash,
+				Size: int(childNode.Size()),
+			})
+		}
+		object := Object{
+			Links: links,
+			Data:  nil,
+		}
+		objectBytes, err1 := json.Marshal(object)
+		if err1 != nil {
+			panic(err1)
+		}
+		objectHash := h.Sum(objectBytes)
+		//保存到store中
+		err2 := store.Put(objectHash, objectBytes)
+		if err2 != nil {
+			panic(err2)
+		}
+		return objectHash
 	default:
 		return nil
 	}
